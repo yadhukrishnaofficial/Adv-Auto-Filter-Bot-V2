bcb6f3abbb08f734941d5e1d8b4f7e766e5368d7
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-07-07 20:50:34 +0530

    🤭

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -10,26 +10,15 @@ from pyrogram.errors import UserNotParticipant
 from bot import FORCESUB_CHANNEL
 
 db = Database()
+FMSG_TXT = """**♦️ READ THIS INSTRUCTION ♦️**
 
+🗣️ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ താഴെ കൊടുത്തിട്ടുള്ള ചാനലിൽ ജോയിൻ ചെയ്യണം. ജോയിൻ ചെയ്ത ശേഷം വീണ്ടും ഗ്രൂപ്പിൽ പോയി ആ ബട്ടനിൽ അമർത്തിയാൽ നിങ്ങൾക്ക് ഞാൻ ആ സിനിമ പ്രൈവറ്റ് ആയി അയച്ചു തരുന്നതാണ്..😍
+
+🗣 In Order To Get The Movie Requested By You in Our Groups, You Will Have To Join Our Official Channel First. After That, Try Accessing That Movie Again From Our Group. I'll Send You That Movie Privately 🙈
+
+👇 𝗖𝗹𝗶𝗰𝗸 𝗧𝗵𝗲 𝗝𝗼𝗶𝗻 & 𝗧𝗿𝘆 👇"""
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
-async def start(bot, update):
-    update_channel = FORCESUB_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Join My Updates Channel to use ME 😎 🤭**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
-            )
-            return
-    
+async def start(bot, update):   
     try:
         file_uid = update.command[1]
     except IndexError:
@@ -44,7 +33,23 @@ async def start(bot, update):
         caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
         
         if file_type == "document":
-        
+            update_channel = FORCESUB_CHANNEL
+            if update_channel:
+                try:
+                    user = await bot.get_chat_member(update_channel, update.chat.id)
+                    if user.status == "kicked":
+                        await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+                        return
+                except UserNotParticipant:
+                    #await update.reply_text(f"Join @{update_channel} To Use Me")
+                    await update.reply_text(
+                        text=FMSG_TXT,
+                        parse_mode="md",
+                        reply_markup=InlineKeyboardMarkup([
+                            [ InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 & 𝗧𝗿𝘆", url=f"https://t.me/{update_channel}")]
+                      ])
+                    )
+                    return
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
@@ -64,7 +69,23 @@ async def start(bot, update):
             )
 
         elif file_type == "video":
-        
+            update_channel = FORCESUB_CHANNEL
+            if update_channel:
+                try:
+                    user = await bot.get_chat_member(update_channel, update.chat.id)
+                    if user.status == "kicked":
+                        await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+                        return
+                except UserNotParticipant:
+                    #await update.reply_text(f"Join @{update_channel} To Use Me")
+                    await update.reply_text(
+                        text=FMSG_TXT,
+                        parse_mode="md",
+                        reply_markup=InlineKeyboardMarkup([
+                            [ InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 & 𝗧𝗿𝘆", url=f"https://t.me/{update_channel}")]
+                      ])
+                    )
+                    return
             await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
@@ -83,7 +104,23 @@ async def start(bot, update):
             )
             
         elif file_type == "audio":
-        
+            update_channel = FORCESUB_CHANNEL
+            if update_channel:
+                try:
+                    user = await bot.get_chat_member(update_channel, update.chat.id)
+                    if user.status == "kicked":
+                        await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+                        return
+                except UserNotParticipant:
+                    #await update.reply_text(f"Join @{update_channel} To Use Me")
+                    await update.reply_text(
+                        text=FMSG_TXT,
+                        parse_mode="md",
+                        reply_markup=InlineKeyboardMarkup([
+                            [ InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 & 𝗧𝗿𝘆", url=f"https://t.me/{update_channel}")]
+                      ])
+                    )
+                    return
             await bot.send_audio(
                 chat_id=update.chat.id,
                 audio = file_id,
